994f4b1f321fe58f78ee8040b547d61ef86a6a95
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-06 12:36:56 +0530

    variables in javascript

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,5 @@
+let = 45;
+const kushal = 45;
+var d = 45;
+
+// Var should be avoided or not to prefer as it gives block scope and functional scope issue.
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert("hello");//we are using node js not browser
+
+console.log(4 + 5);
+console.log("Kushal");
+
+let name = "Kushal";
+let age = 22;
+let isLoggedIn = false;
+let state = null;
+let district;
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+//  symbol => unique
+
+// object
+
+console.log(typeof "Kushal");
+console.log(typeof age);
+
+console.log(typeof undefined); //Undefined
+console.log(typeof null); // Object
