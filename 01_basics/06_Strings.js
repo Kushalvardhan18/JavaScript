de51cd820f73cba2327984f16d5cac696cd0f228
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-10 11:14:46 +0530

    String methods
    Practising Strings method and learning string methods.

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -12,3 +12,20 @@ console.log(fullName.length);
 
 console.log(fullName.charAt(3));
 console.log(fullName.indexOf("h"));
+
+const newString = fullName.substring(0, 6);
+console.log(newString);
+
+const anotherString = fullName.slice(-8, 10);
+console.log(anotherString);
+
+const newStringOne = "      kushal vardhan   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://kushal.com/kushal%20vardhan";
+console.log(url);
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("kushal"));
+console.log(url.split("/"));
