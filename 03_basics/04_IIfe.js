92ca3710b95b7d32a7be8cb9ddb2df4827b58c50
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-13 11:58:44 +0530

    Update 04_IIfe.js

--- 03_basics/04_IIfe.js
+++ 03_basics/04_IIfe.js
@@ -1,1 +1,21 @@
 // Immediatly Invoked Function Expressions
+// to avoid unnecessary values from global scope (avoid global scope pollution)
+//  to immediately execute
+
+function chai() {
+  console.log(`DB connected`);
+}
+chai();
+
+(function chai() {
+  // named IIFE
+  console.log(`DB connected`);
+})();
+
+// ()()
+
+((name) => {
+  //Unnamed IIFe
+  console.log(`Db connected ${name}`);
+})("kushal");
+// semicolon is needed for ending function calls.
