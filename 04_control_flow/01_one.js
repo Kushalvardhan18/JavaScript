663f38bcba49d9a3c6598dd4f2ba09a89a318c62
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-18 14:43:29 +0530

    Create 01_one.js

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,27 @@
+// Control Flow or Logic Flow
+//If statement
+
+const userLoggedIn = true;
+if (userLoggedIn) {
+  console.log("Kushal Vardhan Logged In");
+}
+
+if (2 == "2") {
+  console.log("String");
+}
+
+// ===(Strict equal) is used for type checking also.
+
+if (2 === "2") {
+  console.log("stingss");
+}
+
+const temperature = 41;
+if (temperature < 50) {
+  console.log("temperature less than 50");
+} else {
+  console.log("Greater than 50");
+}
+
+const balance = 1000;
+if (balance > 500) console.log("test"); // Implicit scope
