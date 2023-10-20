d4169710a60d6dec901aeb71fd6f29de572b953f
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-20 16:39:01 +0530

    Update 01_for_loop.js

--- 05_iterations/01_for_loop.js
+++ 05_iterations/01_for_loop.js
@@ -2,4 +2,14 @@
 
 for (let i = 0; i <= 10; i++) {
   console.log(i);
+  if (i == 5) {
+    console.log("5 is Best number");
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Outer loop: ${i}`);
+  for (let j = 0; j <= 10; j++) {
+    console.log(`inner loop: ${j}`);
+  }
 }
