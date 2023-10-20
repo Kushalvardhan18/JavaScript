814f26f3e235e0ca8488e18f9ac842d0d44cc5db
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-20 12:30:44 +0530

    Update 03_truthy.js

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -20,3 +20,18 @@ const emptyObj = {};
 if (Object.keys(emptyObj).length === 0) {
   console.log("Object is empty");
 }
+
+// Nullish Coalescing Operator (??) :null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+val1 = null ?? 44;
+console.log(val1);
+// Used in Databases for keeping the flow.
+
+// Ternary Operator
+// condition ? true : false;
+const iceTeaPrice = 100;
+iceTeaPrice >= 50
+  ? console.log("greater than 50")
+  : console.log("less than 50");
