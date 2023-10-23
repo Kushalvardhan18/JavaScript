fafe570b0635b34dfdea6dcb064642b660e64add
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-23 07:59:56 +0530

    Create 02_while_doWhile_loop.js

---
+++ 05_iterations/02_while_doWhile_loop.js
@@ -0,0 +1,21 @@
+let index = 0;
+while (index <= 10) {
+  console.log(`Value: ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["batman", "superman", "ironman", "spiderman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`Value: ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+// one time will be printed than condition is checked.
