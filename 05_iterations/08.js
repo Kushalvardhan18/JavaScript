b8d62582de109ee3cde4ebc0bf23875ff115d8af
Kushal Vardhan <vardhank140102@gmail.com>
2023-10-25 12:17:19 +0530

    Update 08.js

--- 05_iterations/08.js
+++ 05_iterations/08.js
@@ -1,9 +1,32 @@
 //  Reduce
 const myNums = [1, 2, 3];
 
-const myTotal = myNums.reduce(function (acc, currentVal) {
-  console.log(`Accu ${acc} and currentVal ${currentVal}`);
-  return acc + currentVal;
-}, 0);
+// const myTotal = myNums.reduce(function (acc, currentVal) {
+//   console.log(`Accu ${acc} and currentVal ${currentVal}`);
+//   return acc + currentVal;
+// }, 0);
 
+const myTotal = myNums.reduce((acc, currVal) => acc + currVal, 0);
 console.log(myTotal);
+
+const shoppingCart = [
+  {
+    itemName: "js Course",
+    price: 2999,
+  },
+  {
+    itemName: "App Dev Course",
+    price: 7999,
+  },
+  {
+    itemName: "java Course",
+    price: 999,
+  },
+  {
+    itemName: "Web dev Course",
+    price: 1999,
+  },
+];
+
+const totalPrice = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+console.log(totalPrice);
