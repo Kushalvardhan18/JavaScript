9b3387672e1605c7d739da21982cba6d057b5bab
Kushal Vardhan <vardhank140102@gmail.com>
2024-04-24 09:09:32 +0530

    Update stringManipulation.js

--- Strings/stringManipulation.js
+++ Strings/stringManipulation.js
@@ -7,3 +7,28 @@ function findIndexof(data, target) {
   console.log(data.indexOf(target));
 }
 findIndexof("Kushal Vardhan", "Vardhan");
+
+function findIndexof(data, target) {
+  console.log(data.lastIndexOf(target));
+}
+findIndexof("Kushal Vardhan Madhu Vardhan", "Vardhan");
+
+function slicing(data, start, end) {
+  console.log(data.slice(start, end));
+}
+slicing("Kushal Vardhan", 0, 5);
+
+function repFn(data) {
+  console.log("Original Data :" + data);
+  console.log(data.replace("Barpagga", "Vardhan"));
+}
+repFn("Madhu Barpagga");
+
+function splitFn(data) {
+  console.log("Original Data :" + data);
+  console.log(data.split(" "));
+}
+splitFn("I am a software Developer");
+
+const value = "          kushal vardhan           ";
+console.log(value.trim());
