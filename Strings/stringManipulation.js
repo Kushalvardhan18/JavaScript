690ad1eb46312c10ad6ce5b133d4bb0940c000e0
Kushal Vardhan <vardhank140102@gmail.com>
2024-04-24 08:34:55 +0530

    Create stringManipulation.js

---
+++ Strings/stringManipulation.js
@@ -0,0 +1,9 @@
+function findLength(data) {
+  console.log(data.length);
+}
+findLength("Kushal Vardhan");
+
+function findIndexof(data, target) {
+  console.log(data.indexOf(target));
+}
+findIndexof("Kushal Vardhan", "Vardhan");
