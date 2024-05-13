193a31d3c0e86421afbbbc3fc179043724cdd2f7
Kushal Vardhan <vardhank140102@gmail.com>
2024-05-13 12:06:52 +0530

    Update callBackFunctions.js

--- callBackFunctions.js
+++ callBackFunctions.js
@@ -10,15 +10,35 @@ function displayPassiveSum(data) {
 }
 sumOfNum(4, 6, displayPassiveSum);
 
-//
+// Calling one fn from another function.
 function square(a) {
   return a * a;
 }
 
-function sumOfSquares(a, b) {
-  let val1 = square(a);
-  let val2 = square(b);
+function cube(a) {
+  return a * a * a;
+}
+function add(a) {
+  return a + a + a;
+}
+// function sumOfSquares(a, b) {
+//   let val1 = square(a);
+//   let val2 = square(b);
+//   return val1 + val2;
+// }
+
+// function sumOfCubes(a, b) {
+//   let val1 = cube(a);
+//   let val2 = cube(b);
+//   return val1 + val2;
+// }
+
+// Callback Function ----->
+
+function sumOfSomething(a, b, fn) {
+  const val1 = fn(a);
+  const val2 = fn(b);
   return val1 + val2;
 }
 
-console.log(sumOfSquares(2, 3));
+console.log(sumOfSomething(2, 4, add));
