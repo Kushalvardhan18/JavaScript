f0d18b692a43da431bc671ef23c2ba89d7863e0a
Kushal Vardhan <vardhank140102@gmail.com>
2024-05-18 11:40:08 +0530

    Update 05-promises.js

--- Level2/05-promises.js
+++ Level2/05-promises.js
@@ -7,7 +7,8 @@ let myPromise = new Promise((resolve, reject) => {
   }
 });
 
-//
+// Promise Object represent the eventual completion (or failure) of an asynchornous Operation and its resulting value.
+
 const fs = require("fs");
 function ReadFile() {
   return new Promise(function (resolve) {
@@ -22,3 +23,46 @@ function onDone(data) {
 }
 
 ReadFile().then(onDone);
+
+//
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "chai ", password: "kushal" });
+  }, 1000);
+});
+
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+// Four ---------------------------------------------------------------->
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "chai", password: "445 " });
+    } else {
+      reject("ERROR");
+    }
+  }, 2000);
+});
+
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("The Promise is either resolved or rejected.");
+  });
+
+// Five ---------------------------------------------------------------->
+
+const promiseFive = new Promise(function (resolve, reject) {});
