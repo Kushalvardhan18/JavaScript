39861318fe900eec5d54216e687a13347c52b2b6
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-01 11:24:27 +0530

    Update 06-async-await.js

---
+++ Level2/06-async-await.js
@@ -0,0 +1,22 @@
+function kushalsAsyncfunction() {
+  let p = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("Hi there");
+    }, 1000);
+  });
+  return p;
+}
+
+// function main() {
+//   kushalsAsyncfunction().then(function (value) {
+//     console.log(value);
+//   });
+// }
+
+async function main() {
+  //  no callbacks , no .then syntax
+  let value = await kushalsAsyncfunction();
+  console.log(value);
+}
+
+main();
